5758780b58f59358a9bb1cefcc0b1c621bcae751
mira-kine <mirak0419@gmail.com>
2021-10-12 11:50:20 -0700

    Test for findById failing

--- test/example.test.js
+++ test/example.test.js
@@ -1,11 +1,11 @@
 import { renderMochi } from '../render-mochi.js';
-import { mochis } from '../mochis.js';
+import { mochis } from '../data/mochis.js';
 import { findById } from '../utils.js';
 
 const test = QUnit.test;
 
 test('renderMochi should return an HTML snippet', (expect) => {
-    const expected = `<div class="mochi-item"><h2>calm mochi</h2><img src="./assets/calm-mochi.jpg"><button>add</button><p>Flavor: ube</p><p>Mood: calm</p><p>Price: 1.5</p><hr></div>`;
+    const expected = `<div class="mochi-item"><h2>calm mochi</h2><img src="./assets/calm-mochi.jpg"><button>add</button><p>Flavor: ube</p><p>Mood: calm</p><p>Price: $1.5</p></div>`;
     const calmMochi = mochis[0];
     const actual = renderMochi(calmMochi).outerHTML;
     expect.equal(actual, expected);
