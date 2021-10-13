c899634b31c7926da0f95baf845bb608333179b7
mira-kine <mirak0419@gmail.com>
2021-10-13 13:48:43 -0700

    addButton increasing qty in local storage

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { renderMochi } from './render-mochi.js';
 import { mochis } from './data/mochis.js';
+import { addItem } from './utils.js';
 
 const productList = document.getElementById('product-list');
 
@@ -7,3 +8,16 @@ for (let mochi of mochis){
     const mochiItem = renderMochi(mochi);
     productList.append(mochiItem);
 }
+
+// loop through all the add buttons
+// add an event listener to each one of them
+// on click, call addItem with the button's id
+
+const addButtons = document.querySelectorAll('.add-product');
+for (let addButton of addButtons) {
+    addButton.addEventListener('click', () => {
+        addItem(addButton.id);
+    });
+}
+
+
--- render-mochi.js
+++ render-mochi.js
@@ -18,7 +18,9 @@ export function renderMochi(mochi){
     mochiPrice.textContent = `Price: $${mochi.price}`;
 
     const addProduct = document.createElement('button');
-    addProduct.textContent = 'add';
+    addProduct.textContent = 'Add to cart';
+    addProduct.id = mochi.id;
+    addProduct.classList.add('add-product');
 
     mochiItem.append(mochiHeader, img, addProduct, mochiFlavor, mochiMood, mochiPrice);
     
